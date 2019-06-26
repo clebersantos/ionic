ad430893538cdcc8d6e658cdcdaee3821545d055
Cleber Santos <oclebersantos@gmail.com>
2019-06-26 09:25:49 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [UsuarioService],
 })
 export class HomePage {
+  public usuarios =  [];
+  public usuarioCadastro = {"id": "", "nome": "", "idade": ""};
 
-  constructor() {}
+  constructor(private usuarioService:UsuarioService) {
+    this.getUsuarios();
+  }
 
+  public getUsuarios() {
+    this.usuarioService.findAll().subscribe(response => this.usuarios = response);
+  }
+
+  public popularForm(usuario) {
+    this.usuarioCadastro = usuario;
+  }
+
+  public salvarUsuario() {
+    if (this.usuarioCadastro.id === "") {
+      this.usuarioService.salvar(this.usuarioCadastro).subscribe(response => this.getUsuarios());
+    } else {
+      this.usuarioService.editar(this.usuarioCadastro).subscribe(response => this.getUsuarios());
+    }
+  }
+
+  public deletarUsuario(id) {
+    this.usuarioService.deletar(id).subscribe(response => this.getUsuarios());
+  }
 }
---
+++ src/app/usuario.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  public urlUsuario = "http://localhost:8000/usuario";
+  constructor(public http: HttpClient) { }
+
+  public findAll(): Observable<any> {
+    return this.http.get(this.urlUsuario);
+  }
+
+  public salvar(usuario): Observable<any> {
+    return this.http.post(this.urlUsuario, usuario);
+  }
+
+  public editar(usuario): Observable<any> {
+    return this.http.put(`${this.urlUsuario}/${usuario.id}`, usuario)
+  }
+
+  public deletar(id): Observable<any> {
+    return this.http.delete(`${this.urlUsuario}/${id}`);
+  }
+}
